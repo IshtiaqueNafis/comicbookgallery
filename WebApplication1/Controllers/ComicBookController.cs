9140525f0f501bdaaad45995a62f28c026303434
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-01-14 12:53:18 -0500

    updated comicbook detail view strongly typed

--- WebApplication1/Controllers/ComicBookController.cs
+++ WebApplication1/Controllers/ComicBookController.cs
@@ -1,4 +1,5 @@
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -6,19 +7,24 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider-Man"; //viewBag lets me use the 
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
-            {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+            var comicBook = new ComicBook()
+            { Id=1,
+              SeriesTitle = "The Amazing Spider-Man",
+              IssueNumber = 700,
+              DescriptionHTML = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+              Artists = new Artists[]
+              { 
+                  new Artists() {Role = "Script",Name = "Dan Slott"},
+                  new Artists() {Role = "Pencils",Name = "Humberto Ramos"},
+                  new Artists() {Role = "Inks",Name = "Victor Olazaba"},
+                  new Artists() {Role = "Colors",Name = "Edgar Delgado"},
+                  new Artists() {Role = "Letters",Name = "Chris Eliopoulos"},
+              }
+              
             };
             
-            return View();
+            
+            return View(comicBook); // view will alwways return a comicbook here 
         }
     }
 }
